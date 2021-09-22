6718116628934447d684053bb2e81bdba2c30dda
dave <dave@armchair.mb.ca>
2021-09-22 22:21:51 +0100

    Delete stuff we don't need in the repo.

--- Toolbox/build/docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.reldb.toolbox.events"},{"l":"org.reldb.toolbox.il8n"},{"l":"org.reldb.toolbox.progress"},{"l":"org.reldb.toolbox.types"},{"l":"org.reldb.toolbox.utilities"},{"l":"org.reldb.toolbox.version"}];updateSearchResults();
